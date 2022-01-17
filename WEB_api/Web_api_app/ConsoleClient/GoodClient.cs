d8ce4b1faabd0395ec1c1f5d1a936bec8c2d33a3
Nazar <62303323+KarmazinN@users.noreply.github.com>
2022-01-17 16:45:45 +0200

    Add files via upload

---
+++ WEB_api/Web_api_app/ConsoleClient/GoodClient.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Web_API_app.Models;
+
+namespace ConsoleClient.GoodMenu
+{
+
+    public class GoodClient
+    {
+        private const string path = @"https://localhost:44356/";
+        
+        public void GetGoods()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync(path + "/api/Good").Result;
+                var res = response.Content.ReadAsStringAsync().Result;
+                var rez2 = response.Content.ReadAsAsync<List<Good>>().Result;
+                foreach (Good p in rez2)
+                    Console.WriteLine($"ID {p.Id}    Product Name:  {p.Name}    Price:  {p.Price}   Description :  {p.Descr}   Count:  {p.Count}");
+            }
+        }
+
+        public void GetGood()
+        {
+            using (var client = new HttpClient())
+            {
+                Console.WriteLine("Write id of element which you  want to get");
+                int id = int.Parse(Console.ReadLine());
+                var response = client.GetAsync(path + $"/api/Good/{id}").Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Element with such id not exists");
+                }
+                else
+                {
+                    var res = response.Content.ReadAsStringAsync().Result;
+
+                    var rez2 = response.Content.ReadAsAsync<Good>().Result;
+                    Console.WriteLine($"ID {rez2.Id}   Product Name:  {rez2.Name}    Price:  {rez2.Price}   Description :  {rez2.Descr}   Count:  {rez2.Count}");
+                }
+            }
+        }
+
+        public void CreateGood()
+        {
+            using (var client = new HttpClient())
+            {
+                Console.WriteLine("Creating good:");
+                Console.WriteLine("Write good name");
+                string a = Console.ReadLine();
+                Console.WriteLine("Write good count");
+                int b = int.Parse(Console.ReadLine());
+                Console.WriteLine("Write good price");
+                int c = int.Parse(Console.ReadLine());
+                Console.WriteLine("Write Description");
+                string e = Console.ReadLine();
+                Good good = new Good() {  Name =a, Price =c, Count =b, Descr =e };
+                var response = client.PostAsJsonAsync(path + "/api/Good", good).Result;
+                var statusCode = response.StatusCode.ToString();
+                Console.WriteLine(statusCode.ToString());
+            }
+        }
+
+        public void DeleteGood()
+        {
+            using (var client = new HttpClient())
+            {
+                Console.WriteLine("Write id of element which you want to delete");
+                int id= int.Parse(Console.ReadLine());
+                var response = client.DeleteAsync(path + $"/api/Good/{id}").Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Ivalid id");
+                }
+                else { 
+                    var statusCode = response.StatusCode.ToString();
+                Console.WriteLine(statusCode.ToString());
+                }
+                
+            }
+
+        }
+
+    }
+}
+                
+
+                   
+
+            
+
+                   
+
+        
+
+       
+
+        
+
+        
---
+++ WEB_api/Web_api_app/ConsoleClient/Program.cs
@@ -0,0 +1,83 @@
+﻿using ConsoleClient.GoodMenu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Web_API_app.Models;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+
+        public const string path = @"https://localhost:44356/";
+        static void Main(string[] args)
+        {
+            GoodClient menu = new GoodClient();
+            OrderClient client = new OrderClient();
+            Console.WriteLine("It's storage app");
+            while (true)
+            {
+
+                Console.WriteLine("Select action:");
+                Console.WriteLine("1 - Get all goods\n" +
+                    "2 - Get specific good\n" +
+                    "3 - Create good\n" +
+                    "4 - Delete Good\n" +
+                    "5 - Get all orders\n" +
+                    "6 - Get specific order\n" +
+                    "7 - Create Order\n" +
+                    "8 - Delete Order\n");
+                int a = int.Parse(Console.ReadLine());
+                switch (a)
+                {
+                    case 1:
+                        {
+                            menu.GetGoods();
+                            break;
+                        }
+                    case 2:
+                        {
+                            menu.GetGood();
+                            break;
+                        }
+                    case 3:
+                        {
+                            menu.CreateGood();
+                            break;
+                        }
+                    case 4:
+                        {
+                            menu.DeleteGood();
+                            break;
+                        }
+                    case 5:
+                        {
+                            client.GetOrders();
+                            break;
+                        }
+                    case 6:
+                        {
+                            client.GetOrder();
+                            break;
+                        }
+                    case 7:
+                        {
+                            client.CreateOrder();
+                            break;
+                        }
+                    case 8:
+                        {
+                            client.DeleteOrder();
+                            break;
+                        }
+                   
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
