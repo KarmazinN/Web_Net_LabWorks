c44e0a6fff31cf0e548217b0e9ea4c3358a3c45e
Nazar <62303323+KarmazinN@users.noreply.github.com>
2022-01-17 16:47:40 +0200

    Add files via upload

---
+++ WEB_api/Web_api_app/Web_API_app/Controllers/GoodController.cs
@@ -0,0 +1,98 @@
+﻿using Web_API_app.App_Start;
+using BLL.DTO;
+using BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Web_API_app.Models;
+
+
+namespace Web_API_app.Controllers
+{
+    public class GoodController : ApiController
+    {
+        IService GoodService;
+        public GoodController(IService serv)
+        {
+            GoodService = serv;
+        }
+       
+        // GET api/<controller>
+        public IEnumerable<Good> Get()
+        {
+            var mapper = AutoMapperConfiguration.GetMapper();
+            var goods = mapper.Map<IEnumerable<GoodDTO>, List<Good>>(GoodService.GetGoods());
+            return goods;
+
+        }
+
+        // GET api/<controller>/5
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                var mapper = AutoMapperConfiguration.GetMapper();
+                var good = mapper.Map<GoodDTO, Good>(GoodService.GetGood(id));
+                return Ok(good);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
+            
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public HttpResponseMessage Post([FromBody] Good good)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+            var mapper = AutoMapperConfiguration.GetMapper();
+            var goodDTO = mapper.Map<Good, GoodDTO>(good);
+            try
+            {
+                GoodService.CreateGood(goodDTO);
+                return new HttpResponseMessage(HttpStatusCode.Created);
+            }
+            catch {
+                return  new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+            }
+
+        // PUT api/<controller>/5
+        public IHttpActionResult Put([FromBody] Good good)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Model is not valid");
+            }
+            var mapper = AutoMapperConfiguration.GetMapper();
+            var goodDTO = mapper.Map<Good, GoodDTO>(good);
+            GoodService.UpdateGood(goodDTO); 
+            return Ok(good);
+        }
+
+        
+        [HttpDelete]
+        // DELETE api/<controller>/5
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                GoodService.DeleteGood(id);
+                return Ok(id);
+            }
+            catch (InvalidOperationException e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
---
+++ WEB_api/Web_api_app/Web_API_app/Controllers/OrderController.cs
@@ -0,0 +1,104 @@
+﻿using AutoMapper;
+using BLL.DTO;
+using BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Web_API_app.App_Start;
+using Web_API_app.Models;
+
+namespace Web_API_app.Controllers
+{
+    public class OrderController : ApiController
+    {
+        IService OrderService;
+        public OrderController(IService serv)
+        {
+            OrderService = serv;
+        }
+        // GET api/<controller>
+        public IEnumerable<Order> Get()
+        {
+            var imapper = AutoMapperConfiguration.GetMapper();
+            var orders = imapper.Map<IEnumerable<OrderDTO>, List<Order>>(OrderService.GetOrders());
+            return orders;
+        }
+
+        // GET api/<controller>/5
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                var imapper = AutoMapperConfiguration.GetMapper();
+                var order = imapper.Map<OrderDTO, Order>(OrderService.GetOrder(id));
+
+                return Ok(order);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            }
+
+        // POST api/<controller>
+        [HttpPost]
+        public HttpResponseMessage Post([FromBody]OrderCreateModel createModel)
+        {
+            
+            Order trueOrder = new Order { Email = createModel.Email, Count = createModel.Count, GoodId = createModel.GoodId };
+            
+            //if (!ModelState.IsValid)
+            //    return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            var imapper = AutoMapperConfiguration.GetMapper();
+            var orderDTO = imapper.Map<Order, OrderDTO>(trueOrder);
+            try
+            {
+                OrderService.MakeOrder(orderDTO);
+                return new HttpResponseMessage(HttpStatusCode.Created);
+            }
+            catch
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // PUT api/<controller>/5
+        public IHttpActionResult Put([FromBody] Order order)
+        {
+            if (order == null)
+            {
+                return BadRequest("Order can't be null");
+            }
+            if (!ModelState.IsValid) {
+                return BadRequest("Model is not valid");
+            }
+            try
+            {
+                var mapper = AutoMapperConfiguration.GetMapper();
+                var orderDTO = mapper.Map<Order, OrderDTO>(order);
+                OrderService.ChangeOrderStatus(orderDTO);
+                return Ok(order);
+            }
+            catch(InvalidOperationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE api/<controller>/5
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                OrderService.DeleteOrder(id);
+                return Ok(id);
+            }
+            catch(InvalidOperationException ex) {
+               return BadRequest(ex.Message);
+            }
+        }
+    }
+}
